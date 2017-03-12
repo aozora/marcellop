7094717c375d67fe902822484151794da87cf4a2
aozora <marcello.palmitessa@gmail.com>
2017-03-12 23:00:03 +0100

    wip marcellop v3 with Nuxt.js

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,16 @@
 <template>
-  <footer>
-    Visit our website for more documentation : <a href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
+  <footer class="footer text-center ptb-150">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="left floatleft">
+            <p>Copyright &copy; 2016 Regaltheme.</p>
+          </div>
+          <div class="right floatright">
+            <p>Design by RegalTheme</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
