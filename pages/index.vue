f84e9e3708c134b396ec3ab2b085b7fd4aecafa5
aozora <marcello.palmitessa@gmail.com>
2017-03-18 16:39:34 +0100

    wip marcellop v3 with Nuxt.js

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <my-header></my-header>
-
     <hero></hero>
+    <about></about>
   </div>
 </template>
 
@@ -10,11 +10,13 @@
 <script>
   import MyHeader from '~components/Header.vue'
   import Hero from '~components/Hero.vue'
+  import About from '~components/About.vue'
 
   export default {
     components: {
       MyHeader,
-      Hero
+      Hero,
+      About
     },
 
     mounted () {
