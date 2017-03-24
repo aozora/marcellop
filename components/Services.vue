e0dbf2083c002eadb41515f3764e5fd2be895d27
aozora <marcello.palmitessa@gmail.com>
2017-03-24 21:23:34 +0100

    updated nuxt and npm packages

--- components/Services.vue
+++ components/Services.vue
@@ -2,7 +2,7 @@
   <section id="service" class="service-area bg-color-1">
     <div class="row column">
       <div class="section-title">
-        <h2>My Services</h2>
+        <h2>My Skills</h2>
         <div class="horizontal-line">
           <div class="top"></div>
           <div class="bottom"></div>
@@ -14,7 +14,7 @@
       <div class="service-area__item small-12 medium-6 large-3 column">
         <div class="single-service pt-50 pb-60 plr-20">
           <i class="zmdi zmdi-devices"></i>
-          <h5 class="montserrat weight-medium no-margin">UI/UX DESIGN</h5>
+          <h5 class="montserrat weight-medium no-margin">Live Style Guide &amp; Pattern Libraries</h5>
           <hr class="mtb-15"/>
           <p>There are many variatio <br/> ns of pssages of Lorm available, bu in some form, </p>
         </div>
@@ -22,7 +22,7 @@
       <div class="service-area__item small-12 medium-6 large-3 column">
         <div class="single-service pt-50 pb-60 plr-20">
           <i class="zmdi zmdi-favorite-outline"></i>
-          <h5 class="montserrat weight-medium no-margin">web design</h5>
+          <h5 class="montserrat weight-medium no-margin">Hi-Fi Web Prototypes</h5>
           <hr class="mtb-15"/>
           <p>There are many variatio <br/> ns of pssages of Lorm available, bu in some form, </p>
         </div>
@@ -30,7 +30,7 @@
       <div class="service-area__item small-12 medium-6 large-3 column">
         <div class="single-service pt-50 pb-60 plr-20">
           <i class="zmdi zmdi-filter-center-focus"></i>
-          <h5 class="montserrat weight-medium no-margin">Minimal DESIGN</h5>
+          <h5 class="montserrat weight-medium no-margin">Responsive Web Design</h5>
           <hr class="mtb-15"/>
           <p>There are many variatio ns of pssages of Lorm available, bu in some form, </p>
         </div>
@@ -38,7 +38,15 @@
       <div class="service-area__item small-12 medium-6 large-3 column">
         <div class="single-service pt-50 pb-60 plr-20">
           <i class="zmdi zmdi-language-html5"></i>
-          <h5 class="montserrat weight-medium no-margin">development</h5>
+          <h5 class="montserrat weight-medium no-margin">Team Leading</h5>
+          <hr class="mtb-15"/>
+          <p>There are many variatio ns of pssages of Lorm available, bu in some form, </p>
+        </div>
+      </div>
+      <div class="service-area__item small-12 medium-6 large-3 column">
+        <div class="single-service pt-50 pb-60 plr-20">
+          <i class="zmdi zmdi-language-html5"></i>
+          <h5 class="montserrat weight-medium no-margin">Build reuseable assets &amp; libraries</h5>
           <hr class="mtb-15"/>
           <p>There are many variatio ns of pssages of Lorm available, bu in some form, </p>
         </div>
