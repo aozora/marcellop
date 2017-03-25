dc0a600576454f10874660f03baa4db3ed71c582
aozora <marcello.palmitessa@gmail.com>
2017-03-25 21:19:12 +0100

    wip

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,15 +1,15 @@
 <template>
-  <footer class="footer text-center ptb-150">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="left floatleft">
-            <p>Copyright &copy; 2016 Regaltheme.</p>
-          </div>
-          <div class="right floatright">
-            <p>Design by RegalTheme</p>
-          </div>
-        </div>
+  <footer>
+    <div class="row">
+      <div class="small-12 medium-6 column">
+        <p>Copyright &copy; 2017 Marcello Palmitessa</p>
+        <p>Made with <a href="https://vuejs.org/">Vue.js</a> and <a href="https://nuxtjs.org/">Nuxt</a></p>
+      </div>
+      <div class="small-12 medium-6 column">
+        <ul class="social-links">
+          <li><a href="http://www.twitter.com/marcellopalmit" target="_blank">Twitter</a></li>
+          <li><a href="http://it.linkedin.com/in/marcellopalmitessa" target="_blank">Linkden</a></li>
+        </ul>
       </div>
     </div>
   </footer>
