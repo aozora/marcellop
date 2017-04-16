f779131bfb1146c5325ff0f688b54200a6713fdf
aozora <marcello.palmitessa@gmail.com>
2017-04-16 18:54:05 +0200

    wip

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -12,58 +12,54 @@
       </div>
     </div>
 
-
     <div class="portfolio-grid row small-up-1 medium-up-3">
-      <div class="grid-item column">
-        <div class="portfolio-item overlay light-1 text-center">
-          <img src="/img/portfolio/bootplus.png" alt="bootplus" aria-hidden="true"/>
-          <div class="zoom-icon">
-            <a href="http://aozora.github.io/bootplus/" class="fancybox">
-              <i class="zmdi zmdi-filter-center-focus"></i>
-            </a>
-          </div>
-          <div class="project-title text-left">
-            <h4 class="font-20 montserrat weight-regular capitalize no-margin">Bootplus</h4>
-            <p class="montserratlight">Sleek, intuitive, and powerful Google styled front-end framework for faster and easier web development. Bootplus provides the same basic templates from Bootstrap, adapted with the new "plus" style.</p>
-          </div>
-        </div>
+      <div class="portfolio-grid-item column">
+        <figure>
+          <img src="/img/portfolio/bootplus-screen.jpeg" alt="bootplus" aria-hidden="true"/>
+          <!--<div class="zoom-icon">-->
+          <!--<a href="http://www.marcellop.com/bootplus/" class="fancybox">-->
+          <!--<i class="zmdi zmdi-filter-center-focus"></i>-->
+          <!--</a>-->
+          <!--</div>-->
+          <figcaption>
+            <h4>Bootplus</h4>
+            <p>Bootplus provides the same basic templates from Bootstrap, adapted with the first Google "plus" style.</p>
+          </figcaption>
+        </figure>
       </div>
 
-      <div class="grid-item column">
-        <div class="portfolio-item overlay light-1 text-center">
-          <img src="/img/portfolio/bootmetro.png" alt="bootmetro" aria-hidden="true"/>
-          <div class="title plr-40 text-center">
-            <div class="zoom-icon">
-              <a href="http://aozora.github.io/bootmetro/" class="fancybox">
-                <i class="zmdi zmdi-filter-center-focus"></i>
-              </a>
-            </div>
-          </div>
-          <div class="project-title text-left">
-            <h4 class="font-20 montserrat weight-regular capitalize no-margin">BootMetro</h4>
-            <p class="montserratlight">
-              BootMetro is a web framework with simple and flexible HTML, CSS, and Javascript for web apps that wants to use the Windows 8 Store Style (ex Metro style) style, without the need to run on Windows 8. BootMetro is built on top of Twitter Bootstrap and provide the same UI elements of an original Windows 8 Store app, such Tiles, Charms Bar, Application Bar, and the same Navigation pattern.</p>
-          </div>
-        </div>
+      <div class="portfolio-grid-item column">
+        <figure>
+          <img src="/img/portfolio/bootmetro-screen.jpeg" alt="bootmetro" aria-hidden="true"/>
+          <!--<div class="title plr-40 text-center">-->
+          <!--<div class="zoom-icon">-->
+          <!--<a href="http://www.marcellop.com/bootmetro/" class="fancybox">-->
+          <!--<i class="zmdi zmdi-filter-center-focus"></i>-->
+          <!--</a>-->
+          <!--</div>-->
+          <!--</div>-->
+          <figcaption>
+            <h4>BootMetro</h4>
+            <p>BootMetro is built on top of Twitter Bootstrap and provide the same UI elements of an original Windows 8 Store app, such Tiles, Charms Bar, Application Bar, and the same Navigation pattern.</p>
+          </figcaption>
+        </figure>
       </div>
 
-      <div class="grid-item column">
-        <div class="portfolio-item overlay light-1 text-center">
-          <img src="/img/portfolio/tripandia.png" alt="tripandia" aria-hidden="true"/>
-          <div class="title plr-40 text-center">
-            <div class="zoom-icon">
-              <a class="various" title="Tripandia web site" href="http://www.tripandia.com/">
-                <i class="zmdi zmdi-play"></i>
-              </a>
-            </div>
-            <div class="project-title text-left">
-              <h4 class="font-20 montserrat weight-regular capitalize no-margin">Tripandia</h4>
-              <p class="montserratlight">Tripandia is a new travel social network that let you share your travels and find usefull tips from others travellers.
-Recreating the travel experience is easy and funny, enriching with photos and maps all the visited places, sharing with friends to let them add theirs photos and stories.
-In 2012 I collaborated with the Tripandia team to realize the front-end, first my sahring various mockups, then implementing part of the web design of the project.</p>
-            </div>
-          </div>
-        </div>
+      <div class="portfolio-grid-item column">
+        <figure>
+          <img src="/img/portfolio/tripandia-screen.jpeg" alt="tripandia" aria-hidden="true"/>
+          <!--<div class="title plr-40 text-center">-->
+          <!--<div class="zoom-icon">-->
+          <!--<a class="various" title="Tripandia web site" href="http://www.tripandia.com/">-->
+          <!--<i class="zmdi zmdi-play"></i>-->
+          <!--</a>-->
+          <!--</div>-->
+          <!--</div>-->
+          <figcaption>
+            <h4>Tripandia</h4>
+            <p>Tripandia is a travel social network that let you share your travels and find usefull tips from others travellers. In 2012 I supported the Tripandia team to build the UI.</p>
+          </figcaption>
+        </figure>
       </div>
     </div>
 
