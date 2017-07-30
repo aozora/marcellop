42b1a0f999c3bb90f08231a92af84c937fc8f4c4
aozora <marcello.palmitessa@gmail.com>
2017-07-30 20:45:47 +0200

    wip

--- components/WhatIdo.vue
+++ components/WhatIdo.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="whatido" class="service-area bg-color-1">
+  <section id="whatido" class="service-area">
     <div class="grid-container">
       <div class="section-title">
         <h2 tabindex="0">What I Do</h2>
@@ -12,6 +12,22 @@
       <div class="grid-x grid-margin-x">
         <div class="service-area__item small-12 medium-6 large-3 cell">
           <div>
+            <img src="~assets/img/html5-css3-js.svg" alt="" aria-hidden="true">
+            <h5 tabindex="0" class="montserrat weight-medium no-margin">Modern Front-end Workflow</h5>
+            <hr class="mtb-15"/>
+            <p tabindex="0">There are many variatio <br/> ns of pssages of Lorm available, bu in some form, </p>
+          </div>
+        </div>
+        <div class="service-area__item small-12 medium-6 large-3 cell">
+          <div>
+            <img src="~assets/img/rwd.svg" alt="" aria-hidden="true">
+            <h5 tabindex="0" class="montserrat weight-medium no-margin">Responsive Web Design</h5>
+            <hr class="mtb-15"/>
+            <p tabindex="0">There are many variatio ns of pssages of Lorm available, bu in some form, </p>
+          </div>
+        </div>
+        <div class="service-area__item small-12 medium-6 large-3 cell">
+          <div>
             <img src="~assets/img/puzzle.svg" alt="" aria-hidden="true">
             <h5 tabindex="0" class="montserrat weight-medium no-margin">Live Style Guide &amp; Pattern Libraries</h5>
             <hr/>
@@ -20,36 +36,20 @@
         </div>
         <div class="service-area__item small-12 medium-6 large-3 cell">
           <div>
-            <i aria-hidden="true" class="zmdi zmdi-devices"></i>
-            <h5 tabindex="0" class="montserrat weight-medium no-margin">Hi-Fi Web Prototypes</h5>
-            <hr class="mtb-15"/>
-            <p tabindex="0">There are many variatio <br/> ns of pssages of Lorm available, bu in some form, </p>
-          </div>
-        </div>
-        <div class="service-area__item small-12 medium-6 large-3 cell">
-          <div>
-            <i aria-hidden="true" class="zmdi zmdi-devices"></i>
-            <h5 tabindex="0" class="montserrat weight-medium no-margin">Responsive Web Design</h5>
-            <hr class="mtb-15"/>
-            <p tabindex="0">There are many variatio ns of pssages of Lorm available, bu in some form, </p>
-          </div>
-        </div>
-        <div class="service-area__item small-12 medium-6 large-3 cell">
-          <div>
-            <i class="zmdi zmdi-language-html5"></i>
+            <img src="~assets/img/graduation-cap.svg" alt="" aria-hidden="true">
             <h5 tabindex="0" class="montserrat weight-medium no-margin">Team Leading</h5>
             <hr class="mtb-15"/>
             <p> tabindex="0"There are many variatio ns of pssages of Lorm available, bu in some form, </p>
           </div>
         </div>
-        <div class="service-area__item small-12 medium-6 large-3 cell">
-          <div>
-            <i aria-hidden="true" class="zmdi zmdi-devices"></i>
-            <h5 tabindex="0" class="montserrat weight-medium no-margin">Build reuseable assets &amp; libraries</h5>
-            <hr class="mtb-15"/>
-            <p tabindex="0">There are many variatio ns of pssages of Lorm available, bu in some form, </p>
-          </div>
-        </div>
+        <!--<div class="service-area__item small-12 medium-6 large-3 cell">-->
+          <!--<div>-->
+            <!--<i aria-hidden="true" class="zmdi zmdi-devices"></i>-->
+            <!--<h5 tabindex="0" class="montserrat weight-medium no-margin">Build reuseable assets &amp; libraries</h5>-->
+            <!--<hr class="mtb-15"/>-->
+            <!--<p tabindex="0">There are many variatio ns of pssages of Lorm available, bu in some form, </p>-->
+          <!--</div>-->
+        <!--</div>-->
       </div>
     </div>
   </section>
