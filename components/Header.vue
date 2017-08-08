9a7b4d9e5c9bf2c993d2f82f5f7e15e818ba004d
aozora <marcello.palmitessa@gmail.com>
2017-08-08 07:34:07 +0200

    adding content

--- components/Header.vue
+++ components/Header.vue
@@ -27,8 +27,8 @@
           <ul class="vertical medium-horizontal menu" data-dropdown-menu>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
             <li><a href="#whatido">What I do</a></li>
+            <li><a href="#experience">Experience</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
         </div>
