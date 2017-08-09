cde26050d36c598643dc18f3d5eb87e5f413ebfd
aozora <marcello.palmitessa@gmail.com>
2017-08-09 08:08:47 +0200

    wip content and fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,5 @@
 <template>
   <div class="main-container">
-    <!-- Loading Bar Start -->
-    <!--<div id="loading">-->
-      <!--<div id="loading-center">-->
-        <!--<div id="loading-center-absolute">-->
-          <!--<img src="~assets/img/catforlogo.png" alt="Mim"/>-->
-          <!--<div class="object" id="object_three"></div>-->
-        <!--</div>-->
-      <!--</div>-->
-    <!--</div>-->
-    <!-- Loading Bar End -->
 
     <my-header></my-header>
 
@@ -20,6 +10,7 @@
       <experience></experience>
       <contact></contact>
     </main>
+
     <my-footer/>
 
   </div>
