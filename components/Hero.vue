a6d1a50372db4384cb05f91f2dd2aa64ce5a24e2
aozora <marcello.palmitessa@gmail.com>
2017-10-04 06:55:42 +0200

    wip refactoring

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,20 +1,54 @@
 <template>
-  <section id="home" class="hero grid-container">
-    <div class="grid-x">
-      <div class="small-12 medium-6 cell static">
-        <div class="hero__headings">
-          <h1 tabindex="0">
-            <small>Hello, my name is</small>
-            Marcello Palmitessa
-          </h1>
-          <p>
-            I am a Front-end architect and developer from Italy with a strong focus on responsive web design, performance and accessibilty.
-            I am an advocate to work with designers to shape and imagine the future together.
-          </p>
-        </div>
-      </div>
-      <div class="small-12 medium-6 cell">
-        <div class="hero__image">
+  <section class="hero grid-container">
+    <!--<img-->
+      <!--sizes="(max-width: 2000px) 100vw, 2000px"-->
+      <!--srcset="-->
+<!--marcello_sjhyvm_c_scale,w_768.jpg 768w,-->
+<!--marcello_sjhyvm_c_scale,w_1099.jpg 1099w,-->
+<!--marcello_sjhyvm_c_scale,w_1380.jpg 1380w,-->
+<!--marcello_sjhyvm_c_scale,w_1628.jpg 1628w,-->
+<!--marcello_sjhyvm_c_scale,w_2000.jpg 2000w"-->
+      <!--src="marcello_sjhyvm_c_scale,w_2000.jpg"-->
+      <!--alt="">-->
+    <!---->
+    <picture>
+      <source
+        media="(max-width: 767px)"
+        sizes="(max-width: 1534px) 100vw, 1534px"
+        srcset="
+/img/hero/marcello_sjhyvm_c_scale,w_375.jpg 375w,
+/img/hero/marcello_sjhyvm_c_scale,w_823.jpg 823w,
+/img/hero/marcello_sjhyvm_c_scale,w_1141.jpg 1141w,
+/img/hero/marcello_sjhyvm_c_scale,w_1433.jpg 1433w,
+/img/hero/marcello_sjhyvm_c_scale,w_1534.jpg 1534w">
+      <img
+        sizes="(max-width: 2000px) 100vw, 2000px"
+        srcset="
+/img/hero/marcello_sjhyvm_c_scale,w_768.jpg 768w,
+/img/hero/marcello_sjhyvm_c_scale,w_1099.jpg 1099w,
+/img/hero/marcello_sjhyvm_c_scale,w_1380.jpg 1380w,
+/img/hero/marcello_sjhyvm_c_scale,w_1628.jpg 1628w,
+/img/hero/marcello_sjhyvm_c_scale,w_2000.jpg 2000w"
+        src="/img/hero/marcello_sjhyvm_c_scale,w_2000.jpg"
+        alt="">
+    </picture>
+
+    <div class="hero__headings">
+      <h1>
+        <small>Hello, my name is</small>
+        Marcello Palmitessa
+      </h1>
+      <p>
+        I am a Front-end architect and developer from Italy with a strong focus on responsive web design, performance and accessibilty.
+        I am an advocate to work with designers to shape and imagine the future together.
+      </p>
+    </div>
+
+    <!--<div class="grid-x">-->
+      <!--<div class="small-12 medium-6 cell static">-->
+      <!--</div>-->
+      <!--<div class="small-12 medium-6 cell">-->
+        <!--<div class="hero__image">-->
           <!--<img-->
             <!--sizes="(max-width: 470px) 100vw, 470px"-->
             <!--srcset="-->
@@ -24,8 +58,8 @@
               <!--/img/hero/marcello-w_470.png 470w"-->
             <!--src="/img/hero/marcello-w_470.png"-->
             <!--alt="picture of Marcello, standing and smiling; the picture is in black/white colors, expept for the shirt collar that is coloured.">-->
-        </div>
-      </div>
-    </div>
+        <!--</div>-->
+      <!--</div>-->
+    <!--</div>-->
   </section>
 </template>
