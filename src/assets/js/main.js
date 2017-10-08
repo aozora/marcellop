b9454b1b8dae15e8a59e328b6fccde35ce858c76
aozora <marcello.palmitessa@gmail.com>
2017-10-08 20:53:04 +0200

    total replatforming

---
+++ src/assets/js/main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+// eslint-disable-next-line no-unused-vars
+import whatInput from 'what-input';
+
+window.$ = $;
+
+// // eslint-disable-next-line no-unused-vars
+// import foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+// import './lib/foundation-explicit-pieces';
+
+
+// $(document).foundation();
+
+// on document ready
+$(document).ready(() => {
+});
