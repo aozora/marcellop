2f577a242125026d214315052d9438542673bead
aozora <marcello.palmitessa@gmail.com>
2017-12-26 19:29:47 +0100

    wip svg motion

--- TODO.md
+++ TODO.md
@@ -2,6 +2,7 @@
 
 X font loading
 * fix skip link
+* reduded motion
 * what I do: change layout on large phone/tablet
 * outline/focus style
 X lazyload images with blur
--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,43 +1,40 @@
+// eslint-disable-next-line no-unused-vars
 import anime from 'animejs';
 
 export default {
   init: () => {
-    const mainMenuHeight = '3.2rem';
+    // const mainMenuHeight = '3.2rem';
     const navButton = document.querySelector('.main-menu button');
     const mainMenu = document.querySelector('.main-menu');
-    const shape = document.querySelector('.main-menu__shape');
+    const shape = document.querySelector('.main-menu__shape svg');
     const menuShapeFrom = shape.getAttribute('data-animate-from');
     const menuShapeTo = shape.getAttribute('data-animate-to');
 
     // eslint-disable-next-line func-names
     navButton.addEventListener('click', function () {
       const isExpanded = this.getAttribute('aria-expanded') === 'true'; //  || false;
-      this.setAttribute('aria-expanded', isExpanded);
+      this.setAttribute('aria-expanded', !isExpanded);
 
       // mainMenu.classList.remove('out-from-right', 'in-from-right');
       mainMenu.classList.toggle('open');
       // mainMenu.classList.add(expanded ? 'out-from-right' : 'in-from-right');
 
-      // animate the main menu background shape
-
-      // expand/collapse the menu to full screen/normal size if expanded
+      // // eslint-disable-next-line no-unused-vars
       anime({
-        targets: mainMenu,
-        height: {
-          value: isExpanded ? mainMenuHeight : '100vh',
-          // delay: 100,
-          duration: 700,
-          easing: 'easeInOutQuad'
-        }
-      });
-
-      anime({
-        targets: '.main-menu__shape polygon',
-        // delay: 100,
+        targets: '.main-menu__shape svg polygon',
+        delay: 500,
         duration: 1200,
         easing: 'linear',
         points: isExpanded ? menuShapeFrom : menuShapeTo
       });
+
+      // if (isExpanded) {
+      //   // collapse if already expanded
+      //   // shapeAnimationPromise.finished.then(shapePositionPromise);
+      // } else {
+      //   // expand if already collapsed
+      //   // shapePositionPromise.finished.then(shapeAnimationPromise);
+      // }
 
       // toggle the links to be focusable only when the menu is open
       const menuLinks = document.querySelectorAll('.menu li a');
@@ -45,14 +42,5 @@ export default {
         link.setAttribute('tabindex', !isExpanded ? 0 : -1);
       });
     });
-
-    // const wall = document.getElementById('wall');
-    // // run this on a click or whenever you want
-    // wall.addEventListener('click', () => {
-    //   // eslint-disable-next-line no-console
-    //   console.log('wall clicked');
-    //   wall.beginElement();
-    // });
-
   }
 };
