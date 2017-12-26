5f29bd93d585e90eac1fab7564357b6715f78d79
aozora <marcello.palmitessa@gmail.com>
2017-12-26 13:13:12 +0100

    wip

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,5 @@
+import anime from 'animejs';
+
 export default {
   init: () => {
     const navButton = document.querySelector('.main-menu button');
@@ -9,8 +11,26 @@ export default {
 
       menu.classList.remove('out-from-right', 'in-from-right');
       menu.classList.toggle('open');
-
       menu.classList.add(expanded ? 'out-from-right' : 'in-from-right');
+
+      anime({
+        targets: document.querySelector('.main-menu'),
+        height: {
+          value: '100vh',
+          // delay: 100,
+          duration: 700,
+          easing: 'easeInOutQuad'
+        }
+      });
+
+      anime({
+        // targets: document.getElementById('shape'),
+        targets: '#shape polygon',
+        // delay: 100,
+        duration: 1200,
+        easing: 'linear',
+        points: document.getElementById('shape').getAttribute('data-animate-to')
+      });
 
       // toggle the links to be focusable only when the menu is open
       const menuLinks = document.querySelectorAll('.menu li a');
@@ -18,5 +38,14 @@ export default {
         link.setAttribute('tabindex', !expanded ? 0 : -1);
       });
     });
+
+    // const wall = document.getElementById('wall');
+    // // run this on a click or whenever you want
+    // wall.addEventListener('click', () => {
+    //   // eslint-disable-next-line no-console
+    //   console.log('wall clicked');
+    //   wall.beginElement();
+    // });
+
   }
 };
