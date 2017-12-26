96ac097b4949960a5c4dc29d24790225bf10c96c
aozora <marcello.palmitessa@gmail.com>
2017-12-26 15:19:25 +0100

    wip

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -2,21 +2,29 @@ import anime from 'animejs';
 
 export default {
   init: () => {
+    const mainMenuHeight = '3.2rem';
     const navButton = document.querySelector('.main-menu button');
+    const mainMenu = document.querySelector('.main-menu');
+    const shape = document.querySelector('.main-menu__shape');
+    const menuShapeFrom = shape.getAttribute('data-animate-from');
+    const menuShapeTo = shape.getAttribute('data-animate-to');
+
     // eslint-disable-next-line func-names
     navButton.addEventListener('click', function () {
-      const expanded = this.getAttribute('aria-expanded') === 'true' || false;
-      this.setAttribute('aria-expanded', !expanded);
-      const menu = document.querySelector('.main-menu');
+      const isExpanded = this.getAttribute('aria-expanded') === 'true'; //  || false;
+      this.setAttribute('aria-expanded', isExpanded);
 
-      menu.classList.remove('out-from-right', 'in-from-right');
-      menu.classList.toggle('open');
-      menu.classList.add(expanded ? 'out-from-right' : 'in-from-right');
+      // mainMenu.classList.remove('out-from-right', 'in-from-right');
+      mainMenu.classList.toggle('open');
+      // mainMenu.classList.add(expanded ? 'out-from-right' : 'in-from-right');
 
+      // animate the main menu background shape
+
+      // expand/collapse the menu to full screen/normal size if expanded
       anime({
-        targets: document.querySelector('.main-menu'),
+        targets: mainMenu,
         height: {
-          value: '100vh',
+          value: isExpanded ? mainMenuHeight : '100vh',
           // delay: 100,
           duration: 700,
           easing: 'easeInOutQuad'
@@ -24,18 +32,17 @@ export default {
       });
 
       anime({
-        // targets: document.getElementById('shape'),
-        targets: '#shape polygon',
+        targets: '.main-menu__shape polygon',
         // delay: 100,
         duration: 1200,
         easing: 'linear',
-        points: document.getElementById('shape').getAttribute('data-animate-to')
+        points: isExpanded ? menuShapeFrom : menuShapeTo
       });
 
       // toggle the links to be focusable only when the menu is open
       const menuLinks = document.querySelectorAll('.menu li a');
       menuLinks.forEach((link) => {
-        link.setAttribute('tabindex', !expanded ? 0 : -1);
+        link.setAttribute('tabindex', !isExpanded ? 0 : -1);
       });
     });
 
