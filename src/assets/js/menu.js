d79dc54d506e6930c2797301a24581295c3f3ef1
aozora <marcello.palmitessa@gmail.com>
2017-12-27 17:23:26 +0100

    wip svg motion

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,10 +1,17 @@
-import smoothScroll from './smoothscroll';
+// import smoothScroll from './smoothscroll';
+import TweenLite from 'gsap/TweenLite';
+// eslint-disable-next-line no-unused-vars
+import './gsap/plugins/MorphSVGPlugin';
+import './gsap/plugins/ScrollToPlugin';
 
 export default {
   init: () => {
     // const mainMenuHeight = '3.2rem';
     const navButton = document.querySelector('.main-menu button');
     const mainMenu = document.querySelector('.main-menu');
+    const shape = document.querySelector('.main-menu__shape svg');
+    const menuShapeFrom = shape.getAttribute('data-animate-from');
+    const menuShapeTo = shape.getAttribute('data-animate-to');
 
     // eslint-disable-next-line func-names
     navButton.addEventListener('click', function () {
@@ -13,44 +20,51 @@ export default {
 
       if (isExpanded) {
         // collapse if already expanded
-        document.getElementById('fill-to-shape').beginElement();
-
-        setTimeout(() => {
-          mainMenu.classList.toggle('shaped');
-
-          setTimeout(() => mainMenu.classList.toggle('open'), 500);
-        }, 500);
+        // document.getElementById('fill-to-shape').beginElement();
+        TweenLite.to('#shape-points', 0.5, {
+          morphSVG: { points: menuShapeTo },
+          onComplete: () => {
+            mainMenu.classList.toggle('shaped');
+            setTimeout(() => mainMenu.classList.toggle('open'), 500);
+          }
+        });
       } else {
         // expand if already collapsed
         mainMenu.classList.toggle('open');
         mainMenu.classList.toggle('shaped');
-        document.getElementById('shape-to-fill').beginElement();
+        // document.getElementById('shape-to-fill').beginElement();
+
+        TweenLite.to('#shape-points', 0.5, {
+          morphSVG: { points: menuShapeFrom }
+        });
       }
 
       // init smoothscroll
       const menuLinks = document.querySelectorAll('.menu li a');
       const handleClick = (event) => {
-        event.preventDefault();
+        // event.preventDefault();
         const target = document.getElementById(event.target.hash.substr(1));
 
-        smoothScroll(target, 60);
+        // smoothScroll(target, 60);
+        TweenLite.to(window, 0.5, {
+          scrollTo: { y: event.target.hash, offsetY: 60 },
+          onComplete: () => {
+            target.focus();
 
-        target.focus();
-
-        // close the menu
-        // collapse if already expanded
-        document.getElementById('fill-to-shape').beginElement();
-
-        setTimeout(() => {
-          mainMenu.classList.toggle('shaped');
-
-          setTimeout(() => mainMenu.classList.toggle('open'), 500);
-        }, 500);
-
+            // close the menu
+            // document.getElementById('fill-to-shape').beginElement();
+            TweenLite.to('#shape-points', 0.5, {
+              morphSVG: { points: menuShapeTo },
+              onComplete: () => {
+                mainMenu.classList.toggle('shaped');
+                setTimeout(() => mainMenu.classList.toggle('open'), 500);
+              }
+            });
+          }
+        });
       };
 
       menuLinks.forEach(item => item.addEventListener('click', handleClick));
-
 
       // toggle the links to be focusable only when the menu is open
       menuLinks.forEach((link) => {
