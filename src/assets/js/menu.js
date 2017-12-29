8f93e7b1191733b1fb2644e7c5e30b7370861a43
aozora <marcello.palmitessa@gmail.com>
2017-12-29 12:46:23 +0100

    wip menu animation gsap

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,5 +1,6 @@
 import TweenLite from 'gsap/TweenLite';
 import TimelineLite from 'gsap/TimelineLite';
+import { Back } from 'gsap/EasePack';
 import './gsap/plugins/MorphSVGPlugin';
 import './gsap/plugins/ScrollToPlugin';
 import './gsap/plugins/CSSPlugin';
@@ -7,9 +8,9 @@ import './gsap/plugins/CSSPlugin';
 // const mainMenuHeight = '3.2rem';
 const navButton = document.querySelector('.main-menu button');
 const mainMenu = document.querySelector('.main-menu');
+const menuLinks = document.querySelectorAll('.menu li a');
 const mainMenuShape = document.querySelector('.main-menu__shape');
 const svg = document.querySelector('.main-menu__shape svg');
-// const menuShapeFrom = svg.getAttribute('data-animate-from');
 const menuShapeTo = svg.getAttribute('data-animate-to');
 
 /**
@@ -33,7 +34,14 @@ const timeline = new TimelineLite({
 // add the tweens to the timeline
 timeline
   .to(mainMenuShape, 0.5, { css: { y: '0%' } })
-  .to('#shape-points', 0.5, { morphSVG: { points: menuShapeTo } });
+  .to('#shape-points', 0.5, { morphSVG: { points: menuShapeTo } })
+  .staggerFromTo(
+    menuLinks, 0.35,
+    { css: { x: '100%' }, ease: Back.easeOut },
+    { css: { x: '0%' }, ease: Back.easeOut },
+    0.1,
+    '-=.45'
+  );
 
 /**
  * open/close the mobile menu with gsap animation
@@ -60,6 +68,7 @@ function handleClick(event) {
   event.preventDefault();
   const target = document.getElementById(event.target.hash.substr(1));
 
+  // Smoothscroll (linear for now)
   TweenLite.to(window, 0.5, {
     scrollTo: { y: event.target.hash, offsetY: 60 },
     onComplete: () => {
@@ -76,6 +85,14 @@ export default {
     // set the intial position of the menu shape
     TweenLite.set(mainMenuShape, { css: { y: '-100%' } });
 
+    // set the intial position of the menu links
+    TweenLite.set(menuLinks, { css: { x: '100%' } });
+
+    // register menu links click event handler
+    menuLinks.forEach((link) => {
+      link.addEventListener('click', handleClick);
+    });
+
     // eslint-disable-next-line func-names
     navButton.addEventListener('click', () => {
       const isAlreadyExpanded = navButton.getAttribute('aria-expanded') === 'true'; //  || false;
@@ -83,15 +100,9 @@ export default {
       // toggle the menu
       toggleMenu();
 
-      // init smoothscroll
-      const menuLinks = document.querySelectorAll('.menu li a');
-
-      // menuLinks.forEach(item => item.addEventListener('click', handleClick));
-
       // toggle the links to be focusable only when the menu is open
       menuLinks.forEach((link) => {
         link.setAttribute('tabindex', !isAlreadyExpanded ? 0 : -1);
-        link.addEventListener('click', handleClick);
       });
     });
   }
