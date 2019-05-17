76fe3795748ec1ebf93e457286af004a232fad47
marcellop <marcello.palmitessa@gmail.com>
2019-05-17 13:34:03 +0200

    added netlifycms (w/o gridsome plugin)

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,32 +7,32 @@
 module.exports = {
   siteName: 'marcellop',
   transformers: {
+    //Add markdown support to all file-system sources
     remark: {
       externalLinksTarget: '_blank',
       externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
       anchorClassName: 'icon icon-link',
       plugins: [
-        // ...global plugins
+        // '@gridsome/remark-prismjs'
       ]
     }
   },
   plugins: [
     {
+      // Create posts from markdown files
       use: '@gridsome/source-filesystem',
       options: {
-        path: 'posts/**/*.md',
         typeName: 'Post',
-        remark: {
-          plugins: [
-            // ...local plugins
-          ]
+        path: 'content/posts/*.md',
+        route: '/:slug',
+        refs: {
+          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+          tags: {
+            typeName: 'Tag',
+            route: '/tag/:id',
+            create: true
+          }
         }
-      }
-    },
-    {
-      use: `gridsome-plugin-netlify-cms`,
-      options: {
-        htmlPath: 'src/admin/index.html'
       }
     }
   ]
