7be4aeacfb99867438c9a37a968dd6b6f956f16d
marcellop <marcello.palmitessa@gmail.com>
2019-05-18 17:43:51 +0200

    wip vf

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="layout">
     <header class="header">
+
       <strong>
         <g-link to="/">{{ $static.metaData.siteName }}</g-link>
       </strong>
+
       <nav class="nav">
         <g-link class="nav__link" to="/#home">Home</g-link>
         <g-link class="nav__link" to="/#about">About</g-link>
@@ -12,17 +14,21 @@
         <g-link class="nav__link" to="/#contact">Contact</g-link>
       </nav>
     </header>
-    <slot/>
+
+    <main id="main" role="main">
+      <slot/>
+    </main>
+
     <footer id="footer"></footer>
   </div>
 </template>
 
 <static-query>
-query {
+  query {
   metaData {
-    siteName
+  siteName
   }
-}
+  }
 </static-query>
 
 <!--<style>-->
