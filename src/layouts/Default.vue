77868473f4bb1239e8c877666ef298a5fe1c1525
marcellop <marcello.palmitessa@gmail.com>
2019-05-18 15:14:09 +0200

    wip vf

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -25,30 +25,30 @@ query {
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
+<!--<style>-->
+<!--body {-->
+<!--  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;-->
+<!--  margin:0;-->
+<!--  padding:0;-->
+<!--  line-height: 1.5;-->
+<!--}-->
 
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
+<!--.layout {-->
+<!--  max-width: 760px;-->
+<!--  margin: 0 auto;-->
+<!--  padding-left: 20px;-->
+<!--  padding-right: 20px;-->
+<!--}-->
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
+<!--.header {-->
+<!--  display: flex;-->
+<!--  justify-content: space-between;-->
+<!--  align-items: center;-->
+<!--  margin-bottom: 20px;-->
+<!--  height: 80px;-->
+<!--}-->
 
-.nav__link {
-  margin-left: 20px;
-}
-</style>
+<!--.nav__link {-->
+<!--  margin-left: 20px;-->
+<!--}-->
+<!--</style>-->
