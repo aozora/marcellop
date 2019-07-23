5d4b1f0b7b52ebae570929db84337624be6fec54
marcellop <marcello.palmitessa@gmail.com>
2019-07-23 10:26:49 +0200

    wip apollo caching

--- apollo/apollo-config.js
+++ apollo/apollo-config.js
@@ -1,6 +1,138 @@
-export default function(context){
+/*
+data in schema.json generated with this query:
+
+query IntrospectionQuery {
+  __schema {
+    queryType {
+      name
+    }
+    mutationType {
+      name
+    }
+    subscriptionType {
+      name
+    }
+    types {
+      ...FullType
+    }
+    directives {
+      name
+      description
+      locations
+      args {
+        ...InputValue
+      }
+    }
+  }
+}
+
+fragment FullType on __Type {
+  kind
+  name
+  description
+  fields(includeDeprecated: true) {
+    name
+    description
+    args {
+      ...InputValue
+    }
+    type {
+      ...TypeRef
+    }
+    isDeprecated
+    deprecationReason
+  }
+  inputFields {
+    ...InputValue
+  }
+  interfaces {
+    ...TypeRef
+  }
+  enumValues(includeDeprecated: true) {
+    name
+    description
+    isDeprecated
+    deprecationReason
+  }
+  possibleTypes {
+    ...TypeRef
+  }
+}
+
+fragment InputValue on __InputValue {
+  name
+  description
+  type {
+    ...TypeRef
+  }
+  defaultValue
+}
+
+fragment TypeRef on __Type {
+  kind
+  name
+  ofType {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+*/
+
+// import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
+// import schema from './schema.json';
+// const fragmentMatcher = new IntrospectionFragmentMatcher({
+//   introspectionQueryResultData: schema
+// });
+//
+// export default ({ req, app }) => {
+//   return {
+//     httpEndpoint: 'https://mycoolgraphqlapi.com/api',
+//     getAuth: () => 'Bearer 4ea46ef94c23e12045c7f20bf32ed9',
+//     tokenName: 'apollo-token',
+//     cache: new InMemoryCache({ fragmentMatcher })
+//   };
+// };
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import mockData from './mock-cache/mock';
+const cache = new InMemoryCache();
+
+cache.writeData({
+  data: mockData
+});
+
+export default function (context) {
   return {
     httpEndpoint: 'https://graphql.datocms.com',
-    getAuth: () => 'Bearer 8079c9fe33d6de8049b44e17717ce5'
+    getAuth: () => 'Bearer 8079c9fe33d6de8049b44e17717ce5',
+    cache
+    // // Local Schema
+    // typeDefs = undefined,
+    // // Local Resolvers
+    // resolvers = undefined,
   };
 }
