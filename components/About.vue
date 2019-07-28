3521c951532fa0e41dcea71c444b87432b65e74a
marcellop <marcello.palmitessa@gmail.com>
2019-07-28 16:43:50 +0200

    wip scroll down button

--- components/About.vue
+++ components/About.vue
@@ -1,13 +1,13 @@
 <template>
   <section v-if="about" class="about">
-    <button type="button" class="scroll-down-button">
+    <button type="button" class="scroll-down-button" aria-hidden="true" @click.prevent="scrollDown">
       <svg width="26px" height="32px" viewBox="0 0 26 32" xmlns="http://www.w3.org/2000/svg">
         <title>Scroll the page down</title>
         <path d="M11.783 0v26.856l-9.075-9.08L.78 19.704l11.378 11.378.964.918.963-.92 11.378-11.377-1.927-1.928-9.076 9.081V0z" fill="#FFF" fill-rule="nonzero"/>
       </svg>
     </button>
 
-    <div class="about__container">
+    <div id="about" class="about__container">
 
       <div v-html="about.aboutDescription1"></div>
 
@@ -47,6 +47,12 @@ export default {
     figureVisibilityChanged: function (isVisible, entry) {
       isVisible ? entry.target.classList.add('visible') : entry.target.classList.remove('visible');
       // console.log(entry)
+    },
+
+    scrollDown: function () {
+      document.querySelector('#about').scrollIntoView({
+        behavior: 'smooth'
+      });
     }
   }
 };
