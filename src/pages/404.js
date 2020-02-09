318dd60522cdb17dc198faa4544a2b23f110dfa1
marcellop <marcello.palmitessa@gmail.com>
2020-02-09 16:58:47 +0100

    upgraded packages. Fix styles for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
+import { HelmetDatoCms } from 'gatsby-source-datocms';
 import Layout from '../components/layout';
 import SEO from '../components/Seo';
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <HelmetDatoCms>
+      <body className="error-page"/>
+    </HelmetDatoCms>
+    <SEO title="404: Not found"/>
+    <h1>Cat not found</h1>
+    <section className="hero">
+      <p>
+        <span>The page </span>
+        <span>you&#39;re looking for </span>
+        <span>doesn&#39;t exists...</span>
+      </p>
+    </section>
   </Layout>
 );
 
