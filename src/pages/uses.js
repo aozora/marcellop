a965788a1544d20664c4247019010e1f82e3fcd2
aozora <marcellop@protonmail.com>
2020-07-06 20:26:44 +0200

    feat(uses): completed uses page

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -28,9 +28,9 @@ const Uses = () => {
       {/* <SEO title="Page two" /> */}
       <section className="uses">
         <h1>Uses</h1>
-
+        <p>Here the specifics of some piece of software or hardware I regularly use.</p>
         {uses && uses.allDatoCmsUse && uses.allDatoCmsUse.nodes && uses.allDatoCmsUse.nodes.map((use) => (
-          <article key={use.id} className="post">
+          <article key={use.id} className="uses__use">
             <h2>{use.title}</h2>
             {/* eslint-disable-next-line react/no-danger */}
             <div className="job__description" dangerouslySetInnerHTML={{ __html: use.descriptionNode.childMarkdownRemark.html }}/>
