f4f77c835836823eef92408066c42a0a09c91e13
aozora <marcellop@protonmail.com>
2020-07-13 21:10:23 +0200

    build(nextjs): refactoring for nextjs migration (Jobs)

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({
 }) {
   return (
     <>
-      <Meta meta={page.seo.concat(site.favicon)} />
+      <Meta meta={page.seo ? page.seo.concat(site.favicon) : site.favicon} />
       <a href="#main" tabIndex="0">Skip to main content</a>
       <Alert preview={preview} />
       <Header menu={menu} />
