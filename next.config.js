e33d57315509aaa2658700480f7db8d293cd9cc6
aozora <marcellop@protonmail.com>
2020-07-20 21:14:27 +0200

    fix(build): fixing deploy for netlify (again...)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const path = require('path');
 const fetch = require('node-fetch');
-// const { getAllPostsWithSlug } = require('./lib/api');
-const sitemap = require('nextjs-sitemap-generator');
-
-const API_URL = 'https://graphql.datocms.com';
-const API_TOKEN = process.env.DATOCMS_API_TOKEN;
+// const sitemap = require('nextjs-sitemap-generator');
 
 // sitemap({
 //   baseUrl: 'https://www.marcellop.com',
@@ -12,65 +8,10 @@ const API_TOKEN = process.env.DATOCMS_API_TOKEN;
 //   targetDirectory: 'static/'
 // });
 
-async function fetchAPI(query, { variables, preview } = {}) {
-  const res = await fetch(API_URL + (preview ? '/preview' : ''), {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${API_TOKEN}`
-    },
-    body: JSON.stringify({
-      query,
-      variables
-    })
-  });
-
-  const json = await res.json();
-  if (json.errors) {
-    console.error(json.errors);
-    throw new Error('Failed to fetch API');
-  }
-  return json.data;
-}
-
-async function getAllPostsWithSlug() {
-  const data = fetchAPI(`
-    {
-      allPosts {
-        slug
-      }
-    }
-  `);
-  return data?.allPosts;
-}
-
 module.exports = {
-  // target: 'serverless',
+  target: 'serverless',
   exportTrailingSlash: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   }
-  // async exportPathMap(
-  //   defaultPathMap,
-  //   {
-  //     dev, dir, outDir, distDir, buildId
-  //   }
-  // ) {
-  //   const paths = {
-  //     '/': { page: '/' },
-  //     '/resume': { page: '/resume' },
-  //     '/uses': { page: '/uses' },
-  //     '/writings': { page: '/writings' }
-  //     // '/writings/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-  //   };
-  //
-  //   const allPosts = await getAllPostsWithSlug();
-  //   console.log('*** DEBUG ***', allPosts);
-  //   // eslint-disable-next-line no-return-assign
-  //   allPosts?.map((post) => paths[`/writings/${post.slug}`] = { page: `/writings/${post.slug}`, query: { slug: post.slug } });
-  //
-  //   console.log('*** DEBUG ***', paths);
-  //
-  //   return paths;
-  // }
 };
