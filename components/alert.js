c878e097b5c2577a0e8a910a2547f73d7d0c0280
aozora <marcellop@protonmail.com>
2021-01-02 15:27:18 +0100

    fix: fixed alert visibility when not in preview mode

--- components/alert.js
+++ components/alert.js
@@ -1,18 +1,14 @@
 export default function Alert({ preview }) {
   return (
     <aside className="alert">
-      {preview ? (
-        <>
-          This is page is a preview.&nbsp;
-          <a
-            href="/api/exit-preview"
-            className="underline hover:text-cyan duration-200 transition-colors"
-          >
-            Click here
-          </a>
-          &nbsp;to exit preview mode.
-        </>
-      ) : null}
+      This is page is a preview.&nbsp;
+      <a
+        href="/api/exit-preview"
+        className="underline hover:text-cyan duration-200 transition-colors"
+      >
+        Click here
+      </a>
+      &nbsp;to exit preview mode.
     </aside>
   );
 }
