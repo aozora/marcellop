0029ad8ecc11c3337480761677ecb2819f241360
aozora <marcellop@protonmail.com>
2021-01-06 16:16:19 +0100

    feat: animated title in home

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
+import { letterContainerVariants, letterVariants } from '@/lib/motionShared';
 
 const Hero = ({ hero }) => (
   <section className="hero">
     {hero.hi && <p className="hi">{hero.hi}</p>}
-    <h1>{hero.heading1}</h1>
+    {/* <h1>{hero.heading1}</h1> */}
+    <motion.h1
+      initial="before"
+      animate="after"
+      variants={letterContainerVariants}
+      className="splitting"
+      data-splitting
+      aria-label={hero.heading1}
+    >
+      {hero.heading1.split(' ').map((word, wordIndex) => (
+        <span key={wordIndex} className="word">
+          {Array.from(word).map((char, index) => (
+            <motion.span
+              key={index}
+              variants={letterVariants}
+              aria-hidden="true"
+              className="char"
+              dangerouslySetInnerHTML={{
+                __html: char === ' ' ? '&nbsp;' : char
+              }}
+            />
+          ))}
+          &nbsp;
+        </span>
+      ))}
+    </motion.h1>
 
     <p>
       <span>{hero.heading2A}</span>
