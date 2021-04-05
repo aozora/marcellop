62f4332b48a8c82701778dfd53563ebfdd08c627
aozora <marcellop@protonmail.com>
2021-04-05 14:18:52 +0200

    fix: fixed seo metatags for home and posts

--- pages/writings/index.js
+++ pages/writings/index.js
@@ -43,6 +43,8 @@ export async function getStaticProps({ preview = false }) {
   const menu = (await getAllMenu(preview)) || null;
   const site = (await getSiteData(preview)) || null;
 
+  console.log(page);
+
   await generateRSS(page);
 
   return {
