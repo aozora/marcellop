a75cbc3ad80d0d962dff4bf79abb7ef68e12cb83
aozora <marcellop@protonmail.com>
2021-06-14 08:39:09 +0200

    refactor: refactored home what section, using pics and parallax

--- components/What.js
+++ components/What.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import gsap from 'gsap';
-// import { Image } from 'react-datocms';
-import Image from 'next/image';
 
 const What = ({ whatido }) => {
   const itemsRef = useRef();
@@ -11,10 +9,35 @@ const What = ({ whatido }) => {
   useEffect(() => {
     if (!isClient) return;
 
+    // ScrollTrigger & Parallax for covers
     const slides = document.querySelectorAll('#whatido .whatido__item');
 
     slides.forEach(slide => {
       const imageWrappers = slide.querySelector('.image-wrapper');
+
+      const tl = gsap.timeline({
+        paused: true,
+        scrollTrigger: {
+          trigger: slide,
+          start: '40% 50%' // position of trigger meets the scroller position
+        }
+      });
+
+      tl.from(slide.querySelectorAll('.whatido__title span'), {
+        ease: 'power4',
+        y: '+=5vh',
+        duration: 2.5
+      }).from(
+        slide.querySelectorAll('.whatido__description span'),
+        {
+          y: '-100%',
+          duration: 2,
+          ease: 'power4'
+        },
+        0.4
+      );
+
+      tl.play();
 
       gsap.fromTo(
         imageWrappers,
@@ -46,23 +69,31 @@ const What = ({ whatido }) => {
               <div className="whatido__item">
                 <div className="whatido__item-container">
                   <div className="whatido__item-content">
-                    <p className="whatido__title">{item.title}</p>
-                    <p className="whatido__description">{item.description}</p>
+                    <p className="whatido__title">
+                      <span>{item.title}</span>
+                    </p>
+                    <p className="whatido__description">
+                      <span>{item.description}</span>
+                    </p>
                   </div>
                   <figure>
-                    {/*  <Image */}
-                    {/*    lazyLoad={false} */}
-                    {/*    className="image-wrapper" */}
-                    {/*    data={{ */}
-                    {/*      ...item.cover.responsiveImage */}
-                    {/*    }} */}
-                    {/*  /> */}
                     {item.cover && (
                       <div className="image-wrapper">
-                        <img
-                          alt={item.cover.responsiveImage.alt}
-                          src={item.cover.responsiveImage.src}
-                        />
+                        <picture>
+                          <source
+                            srcSet={item.cover.responsiveImage.webpSrcSet}
+                            sizes={item.cover.responsiveImage.sizes}
+                            type="image/webp"
+                          />
+                          <source
+                            srcSet={item.cover.responsiveImage.srcSet}
+                            sizes={item.cover.responsiveImage.sizes}
+                          />
+                          <img
+                            alt={item.cover.responsiveImage.alt}
+                            src={item.cover.responsiveImage.src}
+                          />
+                        </picture>
                       </div>
                     )}
                   </figure>
