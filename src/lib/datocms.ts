0f2e49cf0fbab3ba7ebc626fe71d352d46c4bb84
aozora <marcellop@protonmail.com>
2022-05-15 09:47:53 +0200

    feat: completed SEO component

--- src/lib/datocms.ts
+++ src/lib/datocms.ts
@@ -1,42 +1,43 @@
 export type RequestType = {
-  query: string;
-  variables?: any;
-  preview?: boolean;
+	query: string;
+	variables?: any;
+	preview?: boolean;
 };
 
 export async function request({ query, variables, preview }: RequestType): Promise<any | null> {
-  let endpoint = 'https://graphql.datocms.com';
+	let endpoint = 'https://graphql.datocms.com';
 
-  if (import.meta.env.VITE_DATOCMS_ENVIRONMENT) {
-    endpoint += `/environments/${import.meta.env.VITE_DATOCMS_ENVIRONMENT}`;
-  }
+	if (import.meta.env.VITE_DATOCMS_ENVIRONMENT) {
+		endpoint += `/environments/${import.meta.env.VITE_DATOCMS_ENVIRONMENT}`;
+	}
 
-  if (preview) {
-    endpoint += `/preview`;
-  }
+	if (preview) {
+		endpoint += `/preview`;
+	}
 
-  // // eslint-disable-next-line no-console
-  // console.log(endpoint);
-  // // eslint-disable-next-line no-console
-  // console.log(import.meta.env.VITE_DATOCMS_API_TOKEN);
+	// // eslint-disable-next-line no-console
+	// console.log(endpoint);
+	// // eslint-disable-next-line no-console
+	// console.log(import.meta.env.VITE_DATOCMS_API_TOKEN);
 
-  const res = await fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${import.meta.env.VITE_DATOCMS_API_TOKEN}`
-    },
-    body: JSON.stringify({
-      query,
-      variables
-    })
-  });
-  const json = await res.json();
+	const res = await fetch(endpoint, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			accept: 'application/json',
+			Authorization: `Bearer ${import.meta.env.VITE_DATOCMS_API_TOKEN}`
+		},
+		body: JSON.stringify({
+			query,
+			variables
+		})
+	});
+	const json = await res.json();
 
-  if (json.errors) {
-    console.error('Ouch! The query has some errors!');
-    throw json.errors;
-  }
+	if (json.errors) {
+		console.error('Ouch! The query has some errors!');
+		throw json.errors;
+	}
 
-  return json.data;
+	return json.data;
 }
