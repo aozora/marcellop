075dc00a23bd914aefd28192975858e7084a30af
aozora <marcellop@protonmail.com>
2022-08-16 16:53:55 +0200

    feat: migrating to latest sveltekit with new routing system chore: removed old dir

--- src/lib/datocms.ts
+++ src/lib/datocms.ts
@@ -5,29 +5,29 @@ export type RequestType = {
 };
 
 export async function cmsRequest({ query, variables, preview }: RequestType): Promise<any | null> {
-	const endpoint = 'https://graphql.datocms.com';
+	let endpoint = 'https://graphql.datocms.com';
 
 	// if (import.meta.env.VITE_DATOCMS_ENVIRONMENT) {
 	// 	endpoint += `/environments/${import.meta.env.VITE_DATOCMS_ENVIRONMENT}`;
 	// }
 	//
-	// if (preview) {
-	// 	endpoint += `/preview`;
-	// }
+	if (preview) {
+		endpoint += `/preview`;
+	}
 
 	// // eslint-disable-next-line no-console
 	// console.log(endpoint);
 	// // eslint-disable-next-line no-console
 	// console.log(import.meta.env.VITE_DATOCMS_API_TOKEN);
-
+	// console.log(JSON.stringify(preview === undefined ? String(false) : String(preview)));
 	const res = await fetch(endpoint, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 			accept: 'application/json',
 			Authorization: `Bearer ${import.meta.env.VITE_DATOCMS_API_TOKEN}`,
-			'X-Environment': import.meta.env.VITE_DATOCMS_ENVIRONMENT,
-			'X-Include-Drafts': String(preview)
+			'X-Environment': import.meta.env.VITE_DATOCMS_ENVIRONMENT
+			// 'X-Include-Drafts': `${preview === undefined ? String(false) : String(preview)}`
 		},
 		body: JSON.stringify({
 			query,
@@ -40,6 +40,8 @@ export async function cmsRequest({ query, variables, preview }: RequestType): Pr
 		console.error('Ouch! The query has some errors!', json.errors);
 		throw json.errors;
 	}
+
+	console.log(json.data);
 
 	return json.data;
 }
