f762d183fe332160df701d40e4aec287a09c1238
aozora <marcellop@protonmail.com>
2023-12-09 12:06:41 +0100

    feat: wip new about

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -87,7 +87,9 @@ onMount(() => {
 			}, '<')
 			.to(self.selector('h2'), {
 				xPercent: 200
-			}, '<');
+			}, '<')
+			.fromTo(lineLeft, { scaleX: 1 }, { scaleX: 0, duration: .35, ease: 'power1.inOut' }, '-=.5')
+			.fromTo(lineRight, { scaleX: 1 }, { scaleX: 0, duration: .35, ease: 'power1.inOut' }, '<');
 
 	}, heroContainer); // <- Scope!
 
@@ -153,69 +155,25 @@ onMount(() => {
 		</h2>
 	</div>
 
-	<!--  <section class="about" use:intersectionAPI on:crossed={(e)=>{-->
-	<!--    updateAboutIsInView(e.detail.isIntersecting);-->
-	<!--  }}>-->
-	<!--    <div id="about" class="about-container">-->
-	<!--      &lt;!&ndash;      <h2>{home.aboutHeading}</h2>&ndash;&gt;-->
+	<div class="about">
+		<div class="about-inner">
+			<h2>{home.aboutHeading}</h2>
 
-	<!--      &lt;!&ndash;      <div class="about-container-textblock-wrapper">&ndash;&gt;-->
-	<!--      &lt;!&ndash;        <p class="dropcap">{@html home.aboutDescription1}</p>&ndash;&gt;-->
-	<!--      &lt;!&ndash;      </div>&ndash;&gt;-->
+			<div class="about-textblock-wrapper">
+				<p>
+					{#each home.aboutDescription1.split(' ') as char}
+						<span>{char}&nbsp;</span>
+					{/each}
+				</p>
+				<p>
+					{#each home.aboutDescription2.split(' ') as char}
+						<span>{char}&nbsp;</span>
+					{/each}
+				</p>
+			</div>
 
-	<!--      <div class="image-container">-->
-	<!--        <svg xmlns="http://www.w3.org/2000/svg" version="1.1">-->
-	<!--          <defs>-->
-	<!--            <filter id="squiggly-0">-->
-	<!--              <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="0" />-->
-	<!--              <feDisplacementMap id="displacement" in="SourceGraphic" in2="noise" scale="6" />-->
-	<!--            </filter>-->
-	<!--            <filter id="squiggly-1">-->
-	<!--              <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="1" />-->
-	<!--              <feDisplacementMap in="SourceGraphic" in2="noise" scale="8" />-->
-	<!--            </filter>-->
-	<!--            <filter id="squiggly-2">-->
-	<!--              <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="2" />-->
-	<!--              <feDisplacementMap in="SourceGraphic" in2="noise" scale="6" />-->
-	<!--            </filter>-->
-	<!--            <filter id="squiggly-3">-->
-	<!--              <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="3" />-->
-	<!--              <feDisplacementMap in="SourceGraphic" in2="noise" scale="8" />-->
-	<!--            </filter>-->
-	<!--            <filter id="squiggly-4">-->
-	<!--              <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="4" />-->
-	<!--              <feDisplacementMap in="SourceGraphic" in2="noise" scale="6" />-->
-	<!--            </filter>-->
-	<!--          </defs>-->
-	<!--        </svg>-->
-
-	<!--        {#each home.aboutPicture as picture}-->
-	<!--          <img loading="lazy"-->
-	<!--               fetchpriority="low"-->
-	<!--               src={picture.responsiveImage.src}-->
-	<!--               srcset={picture.responsiveImage.srcSet}-->
-	<!--               sizes={picture.responsiveImage.sizes}-->
-	<!--               alt={picture.responsiveImage.alt}-->
-	<!--               style="aspect-ratio: {picture.responsiveImage.aspectRatio};"-->
-	<!--          />-->
-	<!--        {/each}-->
-	<!--      </div>-->
-
-	<!--      &lt;!&ndash;      <div class="about-container-textblock-wrapper">&ndash;&gt;-->
-	<!--      &lt;!&ndash;        <p>{@html home.aboutDescription2}</p>&ndash;&gt;-->
-	<!--      &lt;!&ndash;      </div>&ndash;&gt;-->
-
-	<!--      &lt;!&ndash;      <div class="about-container-textblock-wrapper">&ndash;&gt;-->
-	<!--      &lt;!&ndash;        <p>{@html home.aboutDescription3}</p>&ndash;&gt;-->
-	<!--      &lt;!&ndash;      </div>&ndash;&gt;-->
-
-	<!--      &lt;!&ndash;      <div class="about-container-textblock-wrapper">&ndash;&gt;-->
-	<!--      &lt;!&ndash;        <p>{@html home.aboutDescription4}</p>&ndash;&gt;-->
-	<!--      &lt;!&ndash;      </div>&ndash;&gt;-->
-	<!--    </div>-->
-	<!--  </section>-->
-
-	<!--  <What home={home} />-->
+		</div>
+	</div>
 
 </div>
 
@@ -291,13 +249,10 @@ onMount(() => {
 
     h1 {
       position: relative;
-      //display: flex;
-      //flex-direction: column;
       width: 100%;
       max-width: none;
       margin: 0 auto;
       font-family: var(--heading-font-family);
-      //font-size: clamp(2.88rem, 5.4vw + 1.8rem, 6.667rem);
       font-size: clamp(2.88rem, 6.3vw + 1.4rem, 7.1rem);
       line-height: var(--hero-h1-line-height-min);
       text-align: center;
@@ -339,159 +294,54 @@ onMount(() => {
 
   }
 
+
   .about {
     position: relative;
-    width: 100%;
-    max-width: none;
-    padding: 25% 0;
-    //background-color: var(--color-primary);
-    //will-change: clip-path;
-
-    @media (min-width: 64em) {
-      padding: 15% 0;
-    }
+    @include layout-grid;
   }
 
-  .about-container {
-    position: relative;
-    @include container;
-
-    @media (min-width: 64em) {
-      //display: grid;
-      //grid-template-columns: 1fr 1fr;
-      //grid-template-rows: repeat(6, auto); // auto auto auto auto auto;
-      //grid-template-areas: "heading2 heading2" "figure figure" "content1 ." ". content2" "content3 ." ". content4";
-      //background-color: var(--color-black);
-    }
+  .about-inner {
+    grid-column: 2;
 
     h2 {
-      position: relative;
-      grid-area: heading2;
-      justify-self: start;
-      width: 100%;
-      max-width: none;
-      margin-bottom: -6rem;
-      padding: 0;
-      font-size: 20vw;
-      line-height: 1.3;
-      text-align: center;
-      z-index: 2;
-      //color: var(--color-white);
-
-      @media (max-width: 360px) {
-        margin-top: 3rem;
-      }
-    }
-
-    p {
-      width: 100%;
-      margin: 0;
-      //color: var(--color-white);
-      letter-spacing: var(--text-on-dark-letter-spacing);
-
-      &.dropcap {
-        @include dropcap;
-      }
-
-      &:first-line {
-        //--text-weight: 700;
-        font-weight: 500;
-      }
+      font-family: var(--heading-font-family);
+      font-size: 25vw;
+      //font-size: clamp(2.88rem, 6.3vw + 1.4rem, 7.1rem);
+      //line-height: var(--hero-h1-line-height-min);
+      text-align: left;
+      //text-transform: uppercase;
+      @include ligature;
     }
   }
 
-  .image-container {
-    overflow: hidden;
-    position: relative;
-    z-index: 1;
-    margin: 6rem 0;
-    padding: 0;
+	.about-textblock-wrapper {
+		p {
+      display: flex;
+      flex-wrap: wrap;
+      margin-bottom: 4rem;
+      --text-weight: 700;
 
-    @media (min-width: 48em) {
-      display: grid;
-      grid-template-columns: repeat(var(--grid-columns-mobile), 1fr);
-      grid-template-rows: repeat(7, 1fr);
-      grid-gap: 0;
-      gap: 0;
-      align-items: start;
-      width: 100%;
-    }
+      span {
+        --text-weight: 700;
 
-    svg {
-      display: none;
-    }
+        //display: inline-block;
+        font-size: 3.5vw;
+        //font-weight: var(--text-weight);
+        //font-variation-settings: 'wght' var(--text-weight);
+        line-height: 1.2;
+        //transition: font-weight .4s ease-in-out;
 
-    img {
-      border: 8px solid var(--color-white);
+        font-variation-settings: 'wght' 700;
+        --delay: calc((var(--char-index) + 1) * 400ms);
 
-      &:nth-child(2) {
-        grid-column: 1/7;
-        grid-row: 2/7;
-      }
-
-      &:nth-child(3) {
-        grid-column: 5/9;
-        grid-row: 3/-1;
-        z-index: 2;
-      }
-
-      &:nth-child(4) {
-        grid-column: 8/12;
-        grid-row: 1/7;
-      }
-
-      &:hover {
-        animation: squiggly-anim 1s linear;
-        //filter: url('#splash-filter');
+        &:hover {
+          animation: breathe 4000ms infinite both;
+          animation-delay: var(--delay);
+          //--text-weight: 400;
+        }
       }
     }
-  }
-
-  //.image-wrapper {
-  //  padding: 0 0 0 calc(2 * var(--text-base-size));
-  //
-  //  img {
-  //    //max-width: 630px;
-  //    margin: 0 auto;
-  //    object-fit: cover;
-  //    //clip-path: polygon(0% 4.43%, 24.53% 0.5%, 47.1% 4.64%, 73.96% 2.89%, 100% 4.89%, 100.00% 96.68%, 73.53% 100%, 46.73% 95.15%, 21.78% 100%, 0% 95.36%)
-  //  }
-  //
-  //  @media (min-width: 64em) {
-  //    //grid-area: figure;
-  //    margin: 0;
-  //    padding: 0;
-  //  }
-  //}
-
-  .about-container-textblock-wrapper {
-    padding: calc(1 * var(--text-base-size)) 0;
-
-    p {
-      font-size: clamp(1.33rem, 1.9vw + 0.9rem, 2.667rem);
-      line-height: 1.5;
-      margin-bottom: calc(1 * var(--text-base-size));
-    }
-
-    @media (min-width: 64em) {
-      &:nth-child(2) {
-        grid-area: content1;
-      }
-
-      &:nth-child(3) {
-        grid-area: content2;
-      }
-
-      &:nth-child(5) {
-        grid-area: content3;
-      }
-
-      &:nth-child(6) {
-        grid-area: content4;
-      }
-
-    }
-  }
+	}
 
 </style>
 
