54e8e4f63291b03d13d10bc5620989f207404b5a
aozora <marcellop@protonmail.com>
2024-10-24 10:15:44 +0200

    feat: disabled ssr, added vercel adapter

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,6 +3,7 @@ import { cmsRequest, type RequestType } from '$lib/datocms';
 import { commonQuery } from '$queries/_common.query';
 
 export const prerender = true;
+export const ssr = false;
 
 export const load: LayoutServerLoad = async () => {
 	const graphqlRequest: RequestType = {
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
