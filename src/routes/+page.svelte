6cd5c4ec6aa51188be4ccdb6e8f15823c200f40b
aozora <marcellop@protonmail.com>
2024-10-24 10:49:16 +0200

    feat: fixed and forced dark theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,135 +1,135 @@
 <script lang="ts">
-import type { Home } from '$types';
-import { onMount } from 'svelte';
-import gsap from 'gsap/dist/gsap';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-import { getHeroTimeline } from '$lib/animations';
-import AboutPicture1 from '$components/AboutPicture1.svelte';
-import { commonData } from '$lib/store';
-import Head from '$components/Head.svelte';
-import Skills from '$components/Skills.svelte';
+	import type { Home } from '$types';
+	import { onMount } from 'svelte';
+	import gsap from 'gsap/dist/gsap';
+	import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+	import { getHeroTimeline } from '$lib/animations';
+	import AboutPicture1 from '$components/AboutPicture1.svelte';
+	import { commonData } from '$lib/store';
+	import Head from '$components/Head.svelte';
+	import Skills from '$components/Skills.svelte';
 
-if (typeof window !== 'undefined') {
-	gsap.registerPlugin(ScrollTrigger);
-}
+	if (typeof window !== 'undefined') {
+		gsap.registerPlugin(ScrollTrigger);
+	}
 
-type HomeProps = {
-	home: Home,
-	domainUrl: string
-}
+	type HomeProps = {
+		home: Home,
+		domainUrl: string
+	}
 
-/**
- * PROPS
- */
-export let data: HomeProps;
-let { home }: HomeProps = data;
-const { site } = $commonData;
-const headTags = home && site ? home.seo.concat(site.favicon) : [];
+	/**
+	 * PROPS
+	 */
+	export let data: HomeProps;
+	let { home }: HomeProps = data;
+	const { site } = $commonData;
+	const headTags = home && site ? home.seo.concat(site.favicon) : [];
 
-let mainContainer: HTMLDivElement;
+	let mainContainer: HTMLDivElement;
 
-onMount(() => {
-	const ctx = gsap.context((self) => {
-		const chars1 = self.selector('.hero2-title1 .char span');
-		const title1 = self.selector('.hero2-title1');
-		const title2 = self.selector('.hero2-title2');
-		const chars2 = self.selector('.hero2-title2 .char span');
-		const menuTitleChars = Array.from(document.querySelectorAll<HTMLElement>('.menu__title .header-title-char span'));
+	onMount(() => {
+		const ctx = gsap.context((self) => {
+			const chars1 = self.selector('.hero2-title1 .char span');
+			const title1 = self.selector('.hero2-title1');
+			const title2 = self.selector('.hero2-title2');
+			const chars2 = self.selector('.hero2-title2 .char span');
+			const menuTitleChars = Array.from(document.querySelectorAll<HTMLElement>('.menu__title .header-title-char span'));
 
-		const tl = getHeroTimeline(chars1, title1, title2, chars2, menuTitleChars);
-		tl.play();
+			const tl = getHeroTimeline(chars1, title1, title2, chars2, menuTitleChars);
+			tl.play();
 
 
-		/**
-		 * About section animations
-		 */
-			// const aboutContainer = self.selector('.about');
-		const aboutTitle = self.selector('.about h2');
-		// const aboutPicture1 = self.selector('.about svg');
-		const defaultColor = '#333'; // grey
-		const divHighlight = '#fff'; // white
+			/**
+			 * About section animations
+			 */
+			// const storedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+			// const isDarkTheme = storedTheme === 'dark';
+			const aboutTitle = self.selector('.about h2');
+			const defaultColor = '#333'; // grey
+			const divHighlight = '#fff'; // white
 
-		const aboutSections: Array<HTMLElement> = self.selector('.about-highlight[data-animate="1"]');
+			const aboutSections: Array<HTMLElement> = self.selector('.about-highlight[data-animate="1"]');
 
-		gsap.fromTo(aboutTitle,
-			{ x: '100%' },
-			{
-				x: '0%',
-				ease: 'Power1.easeInOut',
-				scrollTrigger: {
-					trigger: aboutTitle,
-					start: 'clamp(top bottom)',
-					end: 'clamp(top top)',
-					toggleActions: 'play none none none',
-					scrub: true
+			gsap.fromTo(aboutTitle,
+				{ x: '100%' },
+				{
+					x: '0%',
+					ease: 'Power1.easeInOut',
+					scrollTrigger: {
+						trigger: aboutTitle,
+						start: 'clamp(top bottom)',
+						end: 'clamp(top top)',
+						toggleActions: 'play none none none',
+						scrub: true
+					}
+				});
+
+			aboutSections.forEach(section => {
+				const wordsTimeline = gsap.timeline();
+				const words = section.querySelectorAll('p .word');
+				const wordsCount = words.length;
+				const picture = section.querySelector('.about svg, .about img');
+				const isPictureOnTheRight = section.classList.contains('text-picture');
+
+				function animateWord(word) {
+					if (wordsScrollTrigger.direction == 1) {
+						gsap.to(word, { color: divHighlight });
+					} else {
+						gsap.to(word, { color: defaultColor });
+					}
 				}
+
+				let wordsScrollTrigger = ScrollTrigger.create({
+					trigger: section,
+					start: 'top 75%',
+					end: 'center 40%',
+					scrub: 1,
+					animation: wordsTimeline
+				});
+
+				words.forEach((word, index) => {
+					wordsTimeline.call(animateWord, [word], index / wordsCount + 0.01);
+				});
+
+				wordsTimeline.to({}, { duration: 0.01 });
+
+				// animate pictures
+				ScrollTrigger.create({
+					trigger: picture,
+					start: 'top 75%',
+					toggleActions: 'play reverse play reverse',
+					// markers: true,
+					animation: gsap.timeline()
+						.fromTo(picture,
+							{
+								autoAlpha: 0,
+								rotate: '-6deg',
+								x: isPictureOnTheRight ? '100%' : '-100%'
+							},
+							{
+								autoAlpha: 1,
+								rotate: '6deg',
+								x: '0%',
+								ease: 'Power1.easeInOut',
+								duration: 1
+							})
+						.to(picture,
+							{
+								rotate: '-6deg',
+								ease: 'Power1.easeInOut',
+								duration: 1
+							})
+				});
 			});
+		}, mainContainer); // <- Scope!
 
-		aboutSections.forEach(section => {
-			const wordsTimeline = gsap.timeline();
-			const words = section.querySelectorAll('p .word');
-			const wordsCount = words.length;
-			const picture = section.querySelector('.about svg, .about img');
-			const isPictureOnTheRight = section.classList.contains('text-picture');
+		return () => ctx.revert(); // <- Cleanup!
+	});
 
-			function animateWord(word) {
-				if (wordsScrollTrigger.direction == 1) {
-					gsap.to(word, { color: divHighlight });
-				} else {
-					gsap.to(word, { color: defaultColor });
-				}
-			}
-
-			let wordsScrollTrigger = ScrollTrigger.create({
-				trigger: section,
-				start: 'top 75%',
-				end: 'center 40%',
-				scrub: 1,
-				animation: wordsTimeline
-			});
-
-			words.forEach((word, index) => {
-				wordsTimeline.call(animateWord, [word], index / wordsCount + 0.01);
-			});
-
-			wordsTimeline.to({}, { duration: 0.01 });
-
-			// animate pictures
-			ScrollTrigger.create({
-				trigger: picture,
-				start: 'top 75%',
-				toggleActions: 'play reverse play reverse',
-				// markers: true,
-				animation: gsap.timeline()
-					.fromTo(picture,
-						{
-							autoAlpha: 0,
-							rotate: '-6deg',
-							x: isPictureOnTheRight ? '100%' : '-100%'
-						},
-						{
-							autoAlpha: 1,
-							rotate: '6deg',
-							x: '0%',
-							ease: 'Power1.easeInOut',
-							duration: 1
-						})
-					.to(picture,
-						{
-							rotate: '-6deg',
-							ease: 'Power1.easeInOut',
-							duration: 1
-						})
-			});
-		});
-	}, mainContainer); // <- Scope!
-
-	return () => ctx.revert(); // <- Cleanup!
-});
-
-const aboutHighlight = 'Yo! I’m a Senior Front-end engineer on a mission to turn digital dreams into delightful realities, with the invaluable support of my cat assistant!';
-const aboutHighlight2 = 'Passionate about crafting efficient, accessible, user-friendly interfaces and scalable architectures while leading development and design teams.';
-const aboutHighlight3 = 'Dedicated to bridging the gap between design and development, leveraging collaborative approaches to create visually stunning and highly functional digital experiences within dynamic team environments.';
+	const aboutHighlight = 'Yo! I’m a Senior Front-end engineer on a mission to turn digital dreams into delightful realities, with the invaluable support of my cat assistant!';
+	const aboutHighlight2 = 'Passionate about crafting efficient, accessible, user-friendly interfaces and scalable architectures while leading development and design teams.';
+	const aboutHighlight3 = 'Dedicated to bridging the gap between design and development, leveraging collaborative approaches to create visually stunning and highly functional digital experiences within dynamic team environments.';
 </script>
 
 <Head {headTags} />
